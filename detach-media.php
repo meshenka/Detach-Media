479e512672584101b7cd1387e04c876eda798752
tweetpressfr <jmaury@beapi.fr>
2014-12-13 19:10:11 +0100

    first commit

---
+++ detach-media.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * Plugin Name: Detach_Media
+ * Plugin URI: https://github.com/TweetPressFr/Detach-Media
+ * Description: Because you can attach media but sometimes you just want to unattach them not delete all the thing !
+ * Version : 1.0
+ * Author: Julien Maury
+ * Author URI: http://tweetpressfr.github.io
+ */
+
+
+/*  Copyright 2014 Julien Maury  (email : contact@tweetpress.fr)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined('ABSPATH')
+    or die("No direct load please !");
+
+if( ! class_exists('Detach_Media') )
+{
+    class Detach_Media
+    {
+        /*
+         *
+         */
+        private static $instance;
+
+        /*
+         *
+         */
+        public static function GetInstance()
+        {
+            if (!isset(self::$instance)){
+                self::$instance = new self();
+            }
+
+            return self::$instance;
+        }
+
+        /*
+         * Use action hook to add our button
+         */
+        public static function hooks()
+        {
+
+            add_action( 'media_row_actions',  array(__CLASS__, 'detach_media'), 10, 3 );
+
+        }
+
+        /*
+         * Allow user to change the post to which the media is attached
+         */
+        public static function detach_media( $actions, $post, $detached )
+        {
+            if ( $post->post_parent != 0 ) {
+
+                if ( current_user_can('edit_post', $post->ID) ) {
+
+                    $actions['attach'] = "<a href='#the-list' onclick='findPosts.open( 'media[]','" . $post->ID . "' );return false;' class='hide-if-no-js'>" . __('Detach', 'detach-media') . "</a>";
+
+                 }
+            }
+
+            return $actions;
+
+        }
+    }
+
+    $Detach_Media = Detach_Media::GetInstance();
+    $Detach_Media->hooks();
+
+}
